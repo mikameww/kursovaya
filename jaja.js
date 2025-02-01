aa4ef5acb3a38dda3038bb06487883f5def349e5
mikameww <152682789+mikameww@users.noreply.github.com>
2025-02-01 21:12:59 +1030

    Create jaja.js

---
+++ jaja.js
@@ -0,0 +1,16 @@
+let sign = prompt("?");
+
+if (sign.toLowerCase() === "Привет") {
+  alert("Привет!");
+}
+
+sign = window.prompt(); 
+sign = prompt(); 
+sign = window.prompt("Этот сайт был вам полезен?", "Да"); 
+sign = window.prompt("Ваше имя?"); 
+sign = window.prompt("Ваше имя.", "Имя"); 
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("demo").innerHTML = "Привет, " + sign;
+});
